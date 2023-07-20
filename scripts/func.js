3d83ca7b92db9d1fff0276b0754da27c13f19cc5
Ankur Shah <ankur3291@gmail.com>
2023-07-20 15:38:07 +0530

    added icons, search and fixed bugs

--- scripts/func.js
+++ scripts/func.js
@@ -22,6 +22,7 @@ const noTask =  document.getElementById('no-task');
 const addTask = document.getElementById('add-task');
 var categories = document.querySelectorAll(".category > span");
 var categoryIndex = 0;          // 0 -> All     1 -> Remaining      2 -> Done
+var search = document.getElementById('search');
 
 // edit page references
 
@@ -108,7 +109,6 @@ title.addEventListener('input', (e)=>{
 
     if(timer) clearTimeout(timer);
     timer = setTimeout(() => {
-        // console.log(e.target.value);
         selectedTask.title = e.target.value;
         localStorage.setItem('tasks', JSON.stringify(tasks));
         localStorage.setItem('selectedTask', JSON.stringify(selectedTask));
@@ -123,10 +123,25 @@ todonote.addEventListener('input', (e)=>{
 
     if(timer) clearTimeout(timer);
     timer = setTimeout(() => {
-        // console.log(e.target.value);
         selectedTask.description = e.target.value;
         localStorage.setItem('tasks', JSON.stringify(tasks));
         localStorage.setItem('selectedTask', JSON.stringify(selectedTask));
+    }, 300);
+});
+
+// search task
+
+search.addEventListener('input', (e)=>{
+    if(timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+        searchTasks();
+    }, 300);
+});
+
+search.addEventListener('search', (e)=>{
+    if(timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+        searchTasks();
     }, 300);
 });
 
@@ -134,7 +149,7 @@ todonote.addEventListener('input', (e)=>{
 function prepareList(){
     if(tasks.length !== 0){
         
-        noTask.style.display = 'none';
+        noTask.classList.add('hide');
         notes.innerHTML = '';
 
         for(let i=0;i<tasks.length;i++){
@@ -144,40 +159,11 @@ function prepareList(){
                 || (categoryIndex == 2 && !tasks[i].status)
             ) continue;
 
-            var task = document.createElement('li');
-            var checkbox = document.createElement('input');
-            var span = document.createElement('span');
-
-            task.classList.add('note');
-            
-            checkbox.type = 'checkbox';
-            checkbox.id = 'check-'+tasks[i].id;
-            checkbox.checked = tasks[i].status;
-            checkbox.addEventListener('change', (cb)=>{
-                tasks[i].status = (cb.target.checked) ? 1 : 0;
-                cb.target.nextSibling.style
-                .textDecorationLine = tasks[i].status ? 'line-through': 'none';
-                // displayTasks();
-                prepareList();
-                localStorage.setItem('tasks', JSON.stringify(tasks));
-            });
-            
-            
-            span.textContent = tasks[i].title;
-            span.style.textDecorationLine = tasks[i].status ? 'line-through': 'none';
-            span.addEventListener('click', (e)=>{
-                e.preventDefault();
-
-                selectedTask = tasks[i];
-                setStage(2);    // preview
-            })
-
-
-            task.appendChild(checkbox);
-            task.appendChild(span);
-
-            notes.appendChild(task);
+            notes.appendChild(createTask(i));
         }
+    }else{
+        notes.innerHTML = '';
+        noTask.classList.remove('hide');
     }
 }
 
@@ -187,19 +173,22 @@ function setStage(stageIndex){
     switch (stages[stageIndex]) {
 
         case 'list':
-
+            
             displayBtns([1,0,0,0]);
             displaySection(0);
+            search.value = '';
 
             if(tasks.length > 0 && !tasks[0].title){
                 tasks.shift();
             }else if(selectedTask && !selectedTask.title){
                 tasks.splice(tasks.map(x=>x.id).indexOf(selectedTask.id), 1);
+            }else if(tasks.length == 0){
+                
             }
             selectedTask = null;
             localStorage.setItem('selectedTask', null);
             localStorage.setItem('tasks', JSON.stringify(tasks));
-
+            
             prepareList();
             
             break;
@@ -219,6 +208,9 @@ function setStage(stageIndex){
                     description: '',
                     status: 0
                 };
+
+                title.value = "";
+                todonote.value = "";
                 
                 tasks.unshift(selectedTask);
                 localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -269,19 +261,75 @@ function displaySection(ind){
     sections[ind].classList.add('selected');
 }
 
-/* function displayTasks(){
-    let taskList = document.querySelectorAll(".notes > .note");
-
-    for(let i=0;i<tasks.length;i++){
-        taskList[i].classList.remove('hide');
-    }
-    if(categoryIndex == 0) return;
-    
-    taskList.forEach((t, i, all)=>{
-        if(categoryIndex == 1){
-            if(tasks[i].status) t.classList.add('hide');
+function searchTasks(){
+    let taskList = Array.from(document.querySelectorAll(".notes > .note"));
+    notes.innerHTML = '';
+    tasks.map((val, i)=>{
+        if(!(val.title.toLowerCase().includes(search.value.toLowerCase()) 
+            || val.description.toLowerCase().includes(search.value.toLowerCase()))
+        ){
+            // taskList[i].classList.add('hide');
+            taskList[i].remove();
         }else{
-            if(!tasks[i].status) t.classList.add('hide');
+            // taskList[i].classList.remove('hide');
+            // taskList[i].append()
+            notes.appendChild(createTask(i));
         }
     })
-} */
+}
+
+function createTask(i){
+    var task = document.createElement('li');
+    var checkbox = document.createElement('input');
+    var span = document.createElement('span');
+    var remove = document.createElement('img');
+
+    task.classList.add('note');
+    
+    checkbox.type = 'checkbox';
+    checkbox.id = 'check-'+tasks[i].id;
+    checkbox.checked = tasks[i].status;
+    checkbox.addEventListener('change', (cb)=>{
+        tasks[i].status = (cb.target.checked) ? 1 : 0;
+        cb.target.nextSibling.style
+        .textDecorationLine = tasks[i].status ? 'line-through': 'none';
+        
+        prepareList();
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    });
+    
+    
+    span.textContent = tasks[i].title;
+    span.style.textDecorationLine = tasks[i].status ? 'line-through': 'none';
+    span.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        selectedTask = tasks[i];
+        setStage(2);    // preview
+    })
+
+    remove.src = '/public/remove.png';
+    remove.addEventListener('click', (e)=>{
+        
+        e.target.parentNode.classList.add('remove');
+        setTimeout(() => {
+            let confirmDelete = confirm('This task will be deleted permanently! Are you sure to proceed?');
+            if(!confirmDelete) {
+                e.target.parentNode.classList.remove('remove');
+                return;
+            }
+
+            tasks.splice(i,1);
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            setStage(0);
+        }, 30);
+        
+    })
+
+
+    task.appendChild(checkbox);
+    task.appendChild(span);
+    task.appendChild(remove);
+
+    return task;
+}
